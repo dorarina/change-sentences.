c1f721cfc5d9c0e9dca08c97056571c2771c8e2e
Darina Nasirova <pqwqlee.dora@gmail.com>
2025-01-14 00:33:32 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# change sentences.
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/dorarina/pen/ByBxQqo](https://codepen.io/dorarina/pen/ByBxQqo).
+
---
+++ script.js
@@ -0,0 +1,16 @@
+function changecolor() {
+  var dd1 = document.getElementById("div1");
+  var dd2 = document.getElementById("div2");
+  
+  dd1.className = "secondbackchange";
+  dd2.className = "firstbackchange";
+  
+}
+
+function changetext() {
+  var dd1 = document.getElementById("div1");
+  var dd2 = document.getElementById("div2");
+  
+  dd1.innerHTML = "be so rooted in yourself that nobodys absence or presence can disturb your peace";
+  dd2.innerHTML = "nothing is permanent. be gratefull for what you have";
+}
